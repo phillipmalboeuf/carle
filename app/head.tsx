cb660637b7a3460f32a59d67308de3bb36a2d236
Phillip Malboeuf <phil@phils.computer>
2023-02-06 15:58:56 -0500

    description

--- app/head.tsx
+++ app/head.tsx
@@ -1,13 +1,13 @@
 export default function Head() {
   return (
     <>
-      <title>AtelierCarle</title>
+      <title>Atelier Carle</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Alain Carle Architecte se dote aujourd’hui d’une nouvelle identité et devient AtelierCarle." />
+      <meta name="description" content="AtelierCarle est une agence d’architecture basée à Montréal depuis plus de 25 ans. La firme, anciennement Alain Carle Architecte, réalise des projets de toutes échelles de type résidentiel, commercial et autres avec un souci marqué pour une conception ancrée dans une pratique théorique. Aujourd’hui composée de 6 associés, la firme exporte son savoir-faire à l’international." />
       <link rel="shortcut icon" href="/favicon.svg" type="image/svg+xml" />
 
-      <meta property="og:title" content="AtelierCarle"/>
-      <meta property="og:description" content="Alain Carle Architecte se dote aujourd’hui d’une nouvelle identité et devient AtelierCarle." />
+      <meta property="og:title" content="Atelier Carle"/>
+      <meta property="og:description" content="AtelierCarle est une agence d’architecture basée à Montréal depuis plus de 25 ans. La firme, anciennement Alain Carle Architecte, réalise des projets de toutes échelles de type résidentiel, commercial et autres avec un souci marqué pour une conception ancrée dans une pratique théorique. Aujourd’hui composée de 6 associés, la firme exporte son savoir-faire à l’international." />
       <meta property="og:type" content="article" />
       <meta property="og:image" content="https://ateliercarle.ca/OG.png"/>
       <meta property="og:url" content="https://ateliercarle.ca"/>
